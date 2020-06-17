88d6ca72802799fa6ce781ce7e93eadf02ed28ea
mohamed albushra <mohamed.albushra@outlook.com>
2020-06-17 15:13:27 +0200

    Updates

---
+++ precache-manifest.8af2eddf352d8c705e7b54345ef06419.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e5877fee99608159076001524d30ce",
+    "url": "/StarWars/index.html"
+  },
+  {
+    "revision": "bb87a5ce3607c92549c1",
+    "url": "/StarWars/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91e022bf5eb49d525924",
+    "url": "/StarWars/static/js/2.624f3b5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StarWars/static/js/2.624f3b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb87a5ce3607c92549c1",
+    "url": "/StarWars/static/js/main.38864386.chunk.js"
+  },
+  {
+    "revision": "c6e7bf0da766930ddc74",
+    "url": "/StarWars/static/js/runtime-main.ded93cbc.js"
+  },
+  {
+    "revision": "8c1b64413fd285bc40f703bbbf600bf2",
+    "url": "/StarWars/static/media/dark.8c1b6441.jpg"
+  },
+  {
+    "revision": "2903413b4721f4d9fe2408bb65a1897a",
+    "url": "/StarWars/static/media/villain.2903413b.svg"
+  }
+]);
